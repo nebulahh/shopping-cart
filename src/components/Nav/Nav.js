5042253fe1aea6e3ca5052e45a7b902b99bd933d
nebulahh <94326584+nebulahh@users.noreply.github.com>
2022-10-12 19:46:39 -0400

    Update Nav.js

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -14,7 +14,7 @@ function Nav() {
 
 <nav className="navbar navbar-expand-lg fixed-top navbar-light bg-light">
             <div className="container px-4 px-lg-5">
-                <Link to={'/'}>
+                <Link to={'/home'}>
                 <span className="navbar-brand" >Anything Store</span>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
